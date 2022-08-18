545a9cb5b691eabd852555f8a260ea5e6c58796d
girik21 <girikaustuv@gmail.com>
2022-08-18 20:05:42 +0545

    first commit

---
+++ news_app/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("News Portal", style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
